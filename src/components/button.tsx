22847f2fa46ad1bda59da58b6fe7616a17756e01
Piotr Berezka <berezkapiotr@gmail.com>
2025-02-18 09:57:38 +0100

    add storybook

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,22 @@
 import { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "link" | "icon" | "destructive";
+  size?: "small" | "medium" | "large";
   className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  size = "medium",
+  className,
+  ...props
+}) => {
   return (
-    <button {...props} className={className}>
+    <button
+      {...props}
+      className={`button ${`button--${variant}`} ${`button--size-${size}`} ${className}`}
+    >
       {props.children}
     </button>
   );
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Button from "../components/button";
+import { Banana } from "lucide-react";
+import "../scss/style.scss";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    // backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {
+    onClick: fn(),
+    children: "Button",
+    variant: "primary",
+    size: "medium",
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {},
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: "link",
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    variant: "icon",
+    children: <Banana />,
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+  },
+};
+
+export const Large: Story = {
+  args: { size: "large" },
+};
+
+export const Medium: Story = {
+  args: { size: "medium" },
+};
+
+export const Small: Story = {
+  args: { size: "small" },
+};
